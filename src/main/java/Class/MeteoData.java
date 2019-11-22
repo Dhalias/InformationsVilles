d2c06156c6c0ec4b71f3cea7883e1bd122b9fb57
Maxime Vannérum <maxime.vannerum@hotmail.com>
2019-11-21 22:39:34 -0500

    Ajout du mapping pour le projet Météo et renommer les méthodes
    Ajout du GetMapping et du PostMapping pour le projet météo afin de permettre l'accès à la page ainsi que l'envoi du formulaire.
    Ajout de la classe MeteoData pour supporter prochainement le data de l'API.

---
+++ src/main/java/Class/MeteoData.java
@@ -0,0 +1,15 @@
+package main.java.Class;
+
+public class MeteoData {
+
+    private String nomVille;
+
+    public String getNomVille() {
+        return nomVille;
+    }
+
+    public void setNomVille(String id) {
+        this.nomVille = id;
+    }
+
+}
--- src/main/java/GreetingController.java
+++ src/main/java/GreetingController.java
@@ -3,15 +3,33 @@ package main.java;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import main.java.Class.MeteoData;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    @RequestMapping(value= {"/accueil","/"} , method = RequestMethod.GET)
+    public String accueil(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "accueil";
+    }
+    
+    
+    @GetMapping("/projet/meteo")
+    public String meteo(Model model){
+    	model.addAttribute("meteoData", new MeteoData());
+    	return "meteo";
+    }
+    
+    @PostMapping("/projet/meteo")
+    public String meteoSubmit(@ModelAttribute MeteoData meteoData) {
+        return "resultMeteo";
     }
 
 }
