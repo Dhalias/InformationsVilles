5e5e9bcb5c3384d02f538583875c629f39287561
Maxime Vannérum <maxime.vannerum@hotmail.com>
2019-11-26 17:04:33 -0500

    PROJET MÉTÉO - Ajout Validation et log console
    Validation : Vérifier que la requete est valide avant d'établir la connexion à l'API. Ajout d'un message qui mentionne à l'usager que la ville entré est invalide.
    Console log : Permet de situer les étapes [HTTP] lors d'une requête à l'API.

--- src/main/java/Class/RequeteAPIMeteo.java
+++ src/main/java/Class/RequeteAPIMeteo.java
@@ -2,6 +2,7 @@ package main.java.Class;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
@@ -9,59 +10,82 @@ import java.util.Scanner;
 
 import org.json.JSONObject;
 
-
 public class RequeteAPIMeteo {
-	
+
 	final String API_KEY = "3246b19ca3e8f14797448968106228aa";
 	final String API_URL = "http://api.openweathermap.org/data/2.5/weather";
 	final String CHARSET = java.nio.charset.StandardCharsets.UTF_8.name();
-	
+
 	private String nomVille;
 	private JSONObject requeteJSON;
-	
-	
-	public RequeteAPIMeteo (String nomVille) throws IOException {
+	private boolean requeteAPI_Succes = false;
+
+	public RequeteAPIMeteo( String nomVille ) throws IOException {
 		this.setNomVille( nomVille );
 		this.effectuerRequeteAPI();
-		
+
 	}
-	
-    
-    private void effectuerRequeteAPI() throws IOException {
-    	
-    	String requete = String.format("q=%s&units=metric&APPID=%s", 
-    		     URLEncoder.encode(this.getNomVille(), CHARSET),
-    		     URLEncoder.encode(API_KEY, CHARSET));
-    	String requeteData;
-    	
-    	URLConnection connexion = new URL(API_URL + "?" + requete).openConnection();
-    	connexion.setRequestProperty( "Accept-Charset", CHARSET );
-    	
-    	InputStream reponse = new URL(API_URL + "?" + requete).openStream();
-    	    	
-    	try (Scanner scanner = new Scanner(reponse)) {
-    	    requeteData = scanner.useDelimiter("\\A").next();
-    	}
-    	
-    	this.setRequeteJSON( new JSONObject(requeteData) );    	
-    	    	
-    }
-	
-    private void setRequeteJSON( JSONObject jsonObject ) {
-		this.requeteJSON = jsonObject;
+
+	private void effectuerRequeteAPI() throws IOException {
+
+		String requete = String.format( "q=%s&units=metric&APPID=%s",
+				URLEncoder.encode( this.getNomVille(), CHARSET ),
+				URLEncoder.encode( API_KEY, CHARSET ) );
+		String requeteData;
+
+		HttpURLConnection connexion = (HttpURLConnection) new URL( API_URL + "?" + requete ).openConnection();
+		connexion.setRequestProperty( "Accept-Charset", CHARSET );
+		connexion.setRequestMethod( "GET" );
 		
+		System.out.println( "\n[HTTP]\tInitialisation a "+( API_URL + "?" + requete ) );
+		System.out.println( "[HTTP]\tCode d'erreur : " + connexion.getResponseCode() );
+
+		if ( connexion.getResponseCode() == 200 ) {
+			
+			connexion.connect();
+			System.out.println( "[HTTP]\tConnexion au site." );
+			
+			InputStream reponse = new URL( API_URL + "?" + requete ).openStream();
+			System.out.println( "[HTTP]\tRecuperation des donnees de la requete." );
+
+			try (Scanner scanner = new Scanner( reponse )) {
+				requeteData = scanner.useDelimiter( "\\A" ).next();
+			}
+
+			this.setRequeteJSON( new JSONObject( requeteData ) );
+			this.requeteAPI_Succes = true;
+			
+			connexion.disconnect();
+			System.out.println( "[HTTP]\tFermeture de la connexion au site." );
+
+		}else {
+			System.out.println( "[HTTP]\tImpossible d'ouvrir la connexion au site. Erreur dans la requete.\n"
+							   +"[HTTP]\tAssurez-vous que le nom de la ville entree est valide." );
+		}
+
+		
+
+	}
+
+	private void setRequeteJSON( JSONObject jsonObject ) {
+		this.requeteJSON = jsonObject;
+
+	}
+
+	public boolean isRequeteAPI_Succes() {
+		return this.requeteAPI_Succes;
 	}
 
 	public JSONObject getRequeteJSON() {
-		return requeteJSON;
+		return this.requeteJSON;
 	}
-	
-	private String getNomVille() {
-        return this.nomVille;
-    }
 
-    private void setNomVille(String nomVille) {
-        this.nomVille = nomVille;
-    }
-	
+	private String getNomVille() {
+		return this.nomVille;
+	}
+
+	private void setNomVille( String nomVille ) {
+		this.nomVille = nomVille;
+	}
+
 }
--- src/main/java/Mapping.java
+++ src/main/java/Mapping.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import main.java.Class.MeteoData;
 import main.java.Class.RequeteAPIMeteo;
@@ -20,47 +21,58 @@ import main.java.Application;
 @Controller
 public class Mapping {
 
-    @RequestMapping(value= {"/accueil","/"} , method = RequestMethod.GET)
-    public String accueil(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "accueil";
-    }
-    
-    
-    @GetMapping("/projet/meteo")
-    public String meteo(Model model){
-    	model.addAttribute("meteoData", new MeteoData());
-    	return "meteo";
-    }
-    
-    @PostMapping("/projet/meteo")
-    public String meteoSubmit(@ModelAttribute MeteoData meteoData, Model model) throws IOException {
-    	
-    	String temperature,pression,path_image,weatherCode,humidite,vent,pays,longitude,latitude;
-    	RequeteAPIMeteo requete = new RequeteAPIMeteo(meteoData.getNomVille());
-    	JSONObject data = requete.getRequeteJSON();
-    	
-    	temperature = data.getJSONObject( "main" ).get( "temp" ).toString();
-    	pression = data.getJSONObject( "main" ).get( "pressure" ).toString();
-    	weatherCode = data.getJSONArray( "weather" ).getJSONObject( 0 ).getString( "main" );
-    	path_image = Application.obtenirImageArrierePlan( weatherCode );
-    	humidite = data.getJSONObject( "main" ).get( "humidity" ).toString();
-    	vent = data.getJSONObject( "wind" ).get( "speed" ).toString();
-    	pays = new Locale("",data.getJSONObject( "sys" ).get( "country" ).toString()).getDisplayCountry();
-    	longitude = data.getJSONObject( "coord" ).get("lon").toString();
-    	latitude = data.getJSONObject( "coord" ).get("lat").toString();
+	@RequestMapping(value = { "/accueil", "/" }, method = RequestMethod.GET)
+	public String accueil( @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model ) {
+		model.addAttribute( "name", name );
+		return "accueil";
+	}
 
-    	
-    	model.addAttribute( "temp", temperature );
-    	model.addAttribute( "pression", pression );
-    	model.addAttribute( "path_image", path_image );
-    	model.addAttribute( "humidite", humidite );
-    	model.addAttribute( "vent", vent );
-    	model.addAttribute( "pays",pays );
-    	model.addAttribute( "longitude",longitude );
-    	model.addAttribute( "latitude", latitude );
-    	
-        return "meteoResultat";
-    }
+	@GetMapping("/projet/meteo")
+	public String meteo( @RequestParam(name = "erreur", required = false) String erreur,Model model ) {
+		model.addAttribute( "erreur", erreur );
+		model.addAttribute( "meteoData", new MeteoData() );
+		return "meteo";
+	}
+
+	@PostMapping("/projet/meteo")
+	public String meteoSubmit( @ModelAttribute MeteoData meteoData, RedirectAttributes attribut, Model model) throws IOException {
+
+		String modeleRendu = "redirect:/projet/meteo";
+		attribut.addAttribute( "erreur","true" );
+
+		RequeteAPIMeteo requete = new RequeteAPIMeteo( meteoData.getNomVille() );
+
+		if ( requete.isRequeteAPI_Succes() ) {
+
+			String temperature, pression, path_image, weatherCode, humidite, vent, pays, longitude, latitude;
+
+			JSONObject data = requete.getRequeteJSON();
+
+			temperature = data.getJSONObject( "main" ).get( "temp" ).toString();
+			pression = data.getJSONObject( "main" ).get( "pressure" ).toString();
+			weatherCode = data.getJSONArray( "weather" ).getJSONObject( 0 ).getString( "main" );
+			path_image = Application.obtenirImageArrierePlan( weatherCode );
+			humidite = data.getJSONObject( "main" ).get( "humidity" ).toString();
+			vent = data.getJSONObject( "wind" ).get( "speed" ).toString();
+			pays = new Locale( "", data.getJSONObject( "sys" ).get( "country" ).toString() ).getDisplayCountry();
+			longitude = data.getJSONObject( "coord" ).get( "lon" ).toString();
+			latitude = data.getJSONObject( "coord" ).get( "lat" ).toString();
+
+			model.addAttribute( "temp", temperature );
+			model.addAttribute( "pression", pression );
+			model.addAttribute( "path_image", path_image );
+			model.addAttribute( "humidite", humidite );
+			model.addAttribute( "vent", vent );
+			model.addAttribute( "pays", pays );
+			model.addAttribute( "longitude", longitude );
+			model.addAttribute( "latitude", latitude );
+
+			modeleRendu = "meteoResultat";
+
+		}
+
+		return modeleRendu;
+	}
 
 }
